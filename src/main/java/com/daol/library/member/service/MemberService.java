c41867a47c3c86590239e5a34c6ad9d2e4364eb7
ysi111 <tjrdls000@naver.com>
2021-11-04 17:58:32 +0900

    1104 작업분
    1104 작업분

--- src/main/java/com/daol/library/member/service/MemberService.java
+++ src/main/java/com/daol/library/member/service/MemberService.java
@@ -1,5 +1,11 @@
 package com.daol.library.member.service;
 
+import com.daol.library.member.domain.Member;
+
 public interface MemberService {
+
+	public int registerMember(Member member);
+
+	public Member loginMember(Member memberOne);
 
 }
--- src/main/java/com/daol/library/member/store/MemberStore.java
+++ src/main/java/com/daol/library/member/store/MemberStore.java
@@ -1,5 +1,11 @@
 package com.daol.library.member.store;
 
+import com.daol.library.member.domain.Member;
+
 public interface MemberStore {
+
+	public int insertMember(Member member);
+
+	public Member selectMember(Member memberOne);
 
 }
