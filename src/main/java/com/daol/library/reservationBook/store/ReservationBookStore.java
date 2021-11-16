74314cf8195b65f30f470075bf3da39460b274a4
Ahram-Jeong <ahram1215@gmail.com>
2021-11-16 18:47:44 +0900

    도서 - 예약 추가, 조회, 삭제

--- src/main/java/com/daol/library/reservationBook/store/ReservationBookStore.java
+++ src/main/java/com/daol/library/reservationBook/store/ReservationBookStore.java
@@ -6,6 +6,7 @@ import com.daol.library.reservationBook.domain.ReservationBook;
 
 public interface ReservationBookStore {
 	public List<ReservationBook> selectAllRsvBook(String userId);
+	public List<ReservationBook> selectAddCheck(ReservationBook reservationBook);
 	public int insertRsv(ReservationBook reservationBook);
 	public int deleteRsv(ReservationBook reservationBook);
 	public int updateRsv(ReservationBook reservationBook);
--- src/main/webapp/resources/js/bookDetail.js
+++ src/main/webapp/resources/js/bookDetail.js
@@ -20,3 +20,26 @@ $("#interesting-btn").click(function(){
         }
     });
 });
+
+$("#reservation-btn").click(function(){
+    let bookNo = '${ book.bookNo }'
+    let userId = '${ sessionScope.userId }'
+    $.ajax({
+        url : "reservationBook.do",
+        type : "get",
+        data : {
+            "bookNo" : bookNo,
+            "userId" : userId
+        },
+        success : function(data) {
+            if(data == "success") {
+                alert("완료");
+            } else {
+                alert("실패");
+            }
+        },
+        error : function() {
+            alert("Ajax 통신 오류");
+        }
+    });
+});
