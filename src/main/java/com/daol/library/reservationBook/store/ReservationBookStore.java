dbb398a556f24b2d018e93c56bf5a288efab8e96
Ahram-Jeong <ahram1215@gmail.com>
2021-11-18 16:59:17 +0900

    도서 예약 - 이메일 알림 발송

--- src/main/java/com/daol/library/reservationBook/store/ReservationBookStore.java
+++ src/main/java/com/daol/library/reservationBook/store/ReservationBookStore.java
@@ -3,14 +3,18 @@ package com.daol.library.reservationBook.store;
 import java.util.List;
 
 import com.daol.library.book.domain.PageInfo;
+import com.daol.library.member.domain.Member;
 import com.daol.library.reservationBook.domain.ReservationBook;
 
 public interface ReservationBookStore {
 	public List<ReservationBook> selectAllRsvBook(PageInfo pi, String userId);
 	public List<ReservationBook> selectAddCheck(ReservationBook reservationBook);
+	public List<ReservationBook> selectRsvList(int bookNo);
+	public ReservationBook selectRsvBookInfo(int bookNo);
+	public List<Member> selectUserEmail(int bookNo);
 	public int insertRsv(ReservationBook reservationBook);
 	public int deleteRsv(ReservationBook reservationBook);
-	public int updateRsv(ReservationBook reservationBook);
+	public int updateRsv(int bookNo);
 //	페이징 처리
 	public int selectListCount(String userId);
 }
